181555766788ff9ad17e2c73b3f7587d5f4b8ece
Matheus Oliveira <matheus-homem2001@hotmail.com>
2025-01-29 22:11:10 -0300

    Inicio de rotas post, patch e delete

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express")
+const rotaLivro = require("./rotas/livro")
 
 const app = express()
 
+app.use('/livros', rotaLivro)
+
 const port = 8000
 
-app.get('/', (req, res) => {
-    res.send('Opa, bão eaí?')
-})
 
 app.listen(port, () => {
     console.log(`Escutando na porta ${port}`)
